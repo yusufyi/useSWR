30ce701cc96badfc971781967ec6ceb38db449a7
Yusuf Yigit <39989409+yusufyi@users.noreply.github.com>
2023-02-20 16:53:06 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import useSWR from "swr";
+
+const fetchAPI = (url) => fetch(url).then((res) => res.json());
 
 function App() {
+  const { data, error, isLoading } = useSWR(
+    "https://api.github.com/repos/vercel/swr",
+    fetchAPI
+  );
+
+  if (error) return "An error has occurred.";
+  if (isLoading) return "Loding..";
+
+  console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data.name}</h1>
+      <h1>{data.description}</h1>
+      <p>{data.subscribers_count}</p>
     </div>
   );
 }
